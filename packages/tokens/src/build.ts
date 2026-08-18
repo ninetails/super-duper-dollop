ab9a88f3162e65b9d7edba344109a16cb1acfc4e
Ninetails <40065+ninetails@users.noreply.github.com>
2026-08-18 11:18:41 -0300

    feat: improved design tokens

--- packages/tokens/src/build.ts
+++ packages/tokens/src/build.ts
@@ -2,19 +2,23 @@ import { StyleDictionary } from 'style-dictionary-utils'
 
 const configs = [
   {
+    include: [
+      'src/tokens/base/global/**/*.json',
+      'src/tokens/base/platform/web/**/*.json',
+    ],
     source: [
-      'src/tokens/base/*.json',
-      'src/tokens/base/web/*.json',
-      'src/tokens/theme/**/*.json',
+      'src/tokens/theme/global/**/*.json',
+      'src/tokens/theme/platform/web/**/*.json',
     ],
     platforms: {
       css: {
         transformGroup: 'css',
-        buildPath: 'dist/css/',
+        buildPath: 'dist/theme/',
         files: [
           {
             destination: 'variables.css',
             format: 'css/variables',
+            filter: 'isSource',
           },
         ],
         transforms: ['clamp/css'],
@@ -22,17 +26,23 @@ const configs = [
     },
   },
   {
-    source: ['src/tokens/base/*.json', 'src/tokens/theme/**/*.json'],
+    include: ['src/tokens/base/global/**/*.json'],
+    source: [
+      'src/tokens/theme/global/**/*.json',
+      'src/tokens/theme/platform/mobile/**/*.json',
+    ],
     platforms: {
       js: {
         transformGroup: 'js',
-        buildPath: 'dist/js/',
+        buildPath: 'dist/theme/',
         files: [
           {
             destination: 'tokens.js',
             format: 'javascript/es6',
+            filter: 'isSource',
           },
         ],
+        transforms: ['name/pathToCamelCase'],
       },
     },
   },
